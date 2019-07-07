256225bc88db798744922e7180eeebc321de4b0a
asanohitoshi <as.hitting.me@gmail.com>
2019-07-07 16:43:29 +0900

    [Finish]rails

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '784371d4d14a6d509bae6f0fce3fbac872288471bc161123540bded3299299119ac2c046beca201aca4e1d2624d816aa2fcb4ed3c59561a8b09e0744be0c9fa0'
